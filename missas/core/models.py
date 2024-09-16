43b0a14dfdf8320fa59e6514e14ea3a94af71563
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2024-09-16 11:00:07 -0300

    fix: Parish.contact can be blank

--- missas/core/models.py
+++ missas/core/models.py
@@ -47,7 +47,9 @@ class Parish(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     city = models.ForeignKey(City, on_delete=models.CASCADE, related_name="parishes")
-    contact = models.OneToOneField(Contact, on_delete=models.CASCADE, null=True)
+    contact = models.OneToOneField(
+        Contact, on_delete=models.CASCADE, blank=True, null=True
+    )
     name = models.CharField(max_length=254)
     slug = models.SlugField(max_length=254)
 
