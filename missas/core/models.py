e2adf3cde1951e0cad86215bf12f0feacb777b13
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2024-09-16 11:11:59 -0300

    fix: Contact.parish can not be null

--- missas/core/models.py
+++ missas/core/models.py
@@ -52,9 +52,7 @@ class Contact(models.Model):
     phone = models.CharField(max_length=16, blank=True)
     phone2 = models.CharField(max_length=16, blank=True)
     whatsapp = models.CharField(max_length=16, blank=True)
-    parish = models.OneToOneField(
-        Parish, on_delete=models.CASCADE, blank=True, null=True
-    )
+    parish = models.OneToOneField(Parish, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.whatsapp or self.phone
