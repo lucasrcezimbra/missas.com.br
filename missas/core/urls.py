0b234ad9471939b783f5bd9d58739e6f7e33fd80
Lucas Rangel Cezimbra <lucas@cezimbra.tec.br>
2025-02-28 21:56:53 -0300

    Add new URL pattern and view for filtering schedules by parish
    * **URLs**
      - Add new URL pattern for parish schedules page
      - Use the `by_parish` view in `missas/core/views.py`
    
    * **View**
      - Add new `by_parish` view to filter schedules by parish
      - Filter schedules by type, day, and time
      - Order schedules by day and start time
      - Use the `parish_schedules.html` template to display schedules by parish

--- missas/core/urls.py
+++ missas/core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<slug:state>/<slug:city>/", views.by_city, name="by_city"),
     path("parish/<slug:parish>/", views.by_parish, name="by_parish"),
+    path("parish/<slug:parish>/schedules/", views.by_parish, name="by_parish"),
     path("<slug:state>/", views.cities_by_state, name="cities_by_state"),
     path("create_contact/", views.create_contact, name="create_contact"),
 ]
